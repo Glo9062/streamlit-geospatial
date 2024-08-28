14e25be06a0c03c0a3c9825e7ed776d9cb7b32ca
Glo111 <94778929+Glo9062@users.noreply.github.com>
2024-08-28 23:51:43 +0800

    Update 5_📍_Marker_Cluster.py

--- pages/5_📍_Marker_Cluster.py
+++ pages/5_📍_Marker_Cluster.py
@@ -5,17 +5,12 @@ st.set_page_config(layout="wide")
 
 st.sidebar.info(
     """
-    - Web App URL: <https://streamlit.gishub.org>
-    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    - Web App URL: <https://app-geo-timelapse.streamlit.app>
+    - GitHub repository: <https://github.com/Glo9062/streamlit-geospatial> 
+    - Forked from: <https://github.com/giswqs/streamlit-geospatial>
     """
 )
 
-st.sidebar.title("Contact")
-st.sidebar.info(
-    """
-    Qiusheng Wu at [wetlands.io](https://wetlands.io) | [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://youtube.com/@giswqs) | [LinkedIn](https://www.linkedin.com/in/giswqs)
-    """
-)
 
 st.title("Marker Cluster")
 
